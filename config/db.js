fcc7588f1422dc07983e512662ec8713c6bbb848
sumitgupta214 <Sumitgupta993577@gmail.com>
2025-07-20 19:32:04 +0530

    Revert "Update db.js"
    This reverts commit 74e16bf063edfab2909e059b523ec668f31be54a.

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mysql=require('mysql2/promise')
 const db = mysql.createPool({
    host: 'localhost',
   user: 'root',
-  password:'Sumit@11',
+  password:'Swati1312@',
   database: 'students_db',
   
 });
